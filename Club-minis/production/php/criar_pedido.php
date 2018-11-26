663c6eec7eaf679c3244fd324f4b4443f146beda
Bruno Artur Torres Lopes Pereira <batlopes@yahoo.com.br>
2018-11-26 18:31:14 -0300

    Registro de pedidos funcionando(com excessão da data)

--- Club-minis/production/php/criar_pedido.php
+++ Club-minis/production/php/criar_pedido.php
@@ -1,24 +1,22 @@
 <?php
-    require_once("./Sql.php");
+    include("Sql.php");
 
     $db = new Sql();
 
-    echo "conectado com sucesso<br/>";
+    echo "conectado com sucesso";
 
     $nome = $_GET["name_client"];
     $prod = $_GET["product"];
     $qtdp = $_GET["qtd_product"];
     $dp = $_GET["data_pedido"];
 
-    print "$nome <br/> $prod <br/> $qtdp <br/> $dp";
 
-    print "<br/>";
-    print "insert into t_pedido (cliente,produto,qtd,data_pedido) values("."null,'".$nome."','".$prod."',".$qtdp.",null)";
+    print "\ninsert into t_pedido(cliente,produto,qtd,dada_pedido) values('".$nome."','".$prod."',".$qtdp.",null)\n";
 
     try{
-        $db->query("insert into t_pedido (cliente,produto,qtd,data_pedido) values("."null,'".$nome."','".$prod."',".$qtdp.",null)");
-    } catch(Exception $e){
-        print $e->collator_get_error_message();
+        $db->query("insert into t_pedido(cliente,produto,qtd,dada_pedido) values('".$nome."','".$prod."',".$qtdp.",null)");
+    } catch(Exception $exception){
+        print $exception->getMessage();
 
     }
 
