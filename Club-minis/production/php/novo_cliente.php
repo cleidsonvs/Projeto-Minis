01796ae745a13afbd974bdfcf513efab5a8b6458
Bruno Artur T L Pereira <batlopes@yahoo.com.br>
2019-01-08 17:41:02 -0300

    Cadastro de cliente adaptado às modificações do banco de dados.

--- Club-minis/production/php/novo_cliente.php
+++ Club-minis/production/php/novo_cliente.php
@@ -1,7 +1,22 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: brunoa
- * Date: 27/11/18
- * Time: 17:55
- */
+include("Sql.php");
+
+$db = new Sql();
+print "Conectado com Sucesso";
+
+$nome = $_GET["nome"];
+$cpf = $_GET["cpf"];
+$adress = $_GET["adress"];
+
+echo "<br>variáveis criadas<br>";
+
+echo "insert into t_cliente(cpf,nome,endereco) values('".$nome."','".$adress."','".$cpf."')";
+
+try{
+    $db->query("insert into t_cliente(cpf,nome,endereco) values('".$nome."','".$adress."','".$cpf."')");
+} catch(Exception $exception){
+    print $exception->getMessage();
+
+}
+
+//header("Location: http://localhost/Projeto-Minis/Club-minis/production/vendas-novo-pedido.php");
